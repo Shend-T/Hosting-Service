5735ec77e5cf30d9db5221c799b211c175a75482
Kapuqin0 <ee.kalludra@gmail.com>
2026-04-10 17:26:16 +0200

    merged eklo into shend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,9 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-<<<<<<< HEAD
 import Header from "./pages/Header";
 import Footer from "./pages/Footer";
-
 import Home from "./pages/Home/Home";
-=======
-import Home from "./pages/Home";
->>>>>>> eklo
 
 import Klienti from "./pages/Klienti/Klienti";
 import Paketa from "./pages/Paketa/Paketa";
@@ -17,21 +12,14 @@ import Abonimi from "./pages/Abonimi/Abonimi";
 function App() {
   return (
     <BrowserRouter>
-<<<<<<< HEAD
       <Header />
-=======
->>>>>>> eklo
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* CRUD-et */}
-        <Route path="/klienti" element={<Klienti />} /> {/* Klienti CRUD */}
-        <Route path="/paketa" element={<Paketa />} /> {/* Paketa CRUD */}
-        <Route path="/abonimi" element={<Abonimi />} /> {/* Abonimi CRUD */}
+        <Route path="/klienti" element={<Klienti />} />
+        <Route path="/paketa" element={<Paketa />} />
+        <Route path="/abonimi" element={<Abonimi />} />
       </Routes>
-<<<<<<< HEAD
       <Footer />
-=======
->>>>>>> eklo
     </BrowserRouter>
   );
 }
