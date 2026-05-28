42688f95778a880281e77f2a0687e739acc38c95
Kapuqin0 <ee.kalludra@gmail.com>
2026-05-28 05:03:04 +0200

    admin controller tiketa pergjigjet faturat

--- backend/app/Http/Controllers/AdminController.php
+++ backend/app/Http/Controllers/AdminController.php
@@ -6,6 +6,9 @@ use Illuminate\Http\Request;
 use App\Models\Klienti;
 use App\Models\Paketa;
 use App\Models\Abonimi;
+use App\Models\Tiketi;
+use App\Models\PergjigjaTiketi;
+use App\Models\Faktura;
 
 class AdminController extends Controller
 {
@@ -18,7 +21,6 @@ class AdminController extends Controller
 
     public function getKlienti(Request $request, int $id) {
         $klienti = Klienti::findOrFail($id);
-
         return response()->json($klienti, 200);
     }
 
@@ -36,7 +38,6 @@ class AdminController extends Controller
         ]);
         
         $klienti = Klienti::create($data);
-
         return response()->json($klienti, 201);
     }
 
@@ -55,14 +56,12 @@ class AdminController extends Controller
         ]);
 
         $klienti->update($data);
-
         return response()->json($klienti, 200);
     }
 
     public function deleteKlienti(Request $request, int $id) {
         $klienti = Klienti::findOrFail($id);
         $klienti->delete();
-
         return response()->json("", 204);
     }
 
@@ -71,9 +70,9 @@ class AdminController extends Controller
         $paketat = Paketa::all();
         return response()->json($paketat, 200);
     }
+
     public function getPaketa(Request $request, int $id) {
         $paketa = Paketa::findOrFail($id);
-
         return response()->json($paketa, 200);
     }
 
@@ -92,7 +91,6 @@ class AdminController extends Controller
         ]);
 
         $paketa = Paketa::create($data);
-
         return response()->json($paketa, 201);
     }
 
@@ -113,27 +111,26 @@ class AdminController extends Controller
         ]);
 
         $paketa->update($data);
-
         return response()->json($paketa, 200);
     }
 
     public function deletePaketa(Request $request, int $id) {
         $paketa = Paketa::findOrFail($id);
         $paketa->delete();
-
         return response()->json("", 204);
     }
 
-    //  ========== Abonimi ==========
+    // ========== Abonimi ==========
     public function getAllAbonimi(Request $request) {
         $abonimet = Abonimi::all();
         return response()->json($abonimet, 200);
     }
+
     public function getAbonimi(Request $request, int $id) {
         $abonimi = Abonimi::findOrFail($id);
-
         return response()->json($abonimi, 200);
     }
+
     public function createAbonimi(Request $request) {
         $data = $request->validate([
             'klienti_id'    => 'required|exists:klienti,id',
@@ -153,7 +150,115 @@ class AdminController extends Controller
     public function deleteAbonimi(Request $request, int $id) {
         $abonimi = Abonimi::findOrFail($id);
         $abonimi->delete();
+        return response()->json("", 204);
+    }
 
+    // ========== Tiketa ==========
+    public function getAllTiketa(Request $request) {
+        $tiketa = Tiketi::with('klienti')->get();
+        return response()->json($tiketa, 200);
+    }
+
+    public function getTiketi(Request $request, int $id) {
+        $tiketi = Tiketi::with(['klienti', 'pergjigjet'])->findOrFail($id);
+        return response()->json($tiketi, 200);
+    }
+
+    public function createTiketi(Request $request) {
+        $data = $request->validate([
+            'klienti_id'  => 'required|exists:klienti,id',
+            'titulli'     => 'required|string|max:255',
+            'pershkrimi'  => 'required|string',
+            'prioriteti'  => 'sometimes|in:i_ulet,normal,i_larte,urgjent',
+            'statusi'     => 'sometimes|in:hapur,ne_proces,mbyllur',
+            'kategoria'   => 'required|string',
+        ]);
+        $tiketi = Tiketi::create($data);
+        return response()->json($tiketi, 201);
+    }
+
+    public function updateTiketi(Request $request, int $id) {
+        $tiketi = Tiketi::findOrFail($id);
+        $data = $request->validate([
+            'titulli'    => 'sometimes|string|max:255',
+            'pershkrimi' => 'sometimes|string',
+            'prioriteti' => 'sometimes|in:i_ulet,normal,i_larte,urgjent',
+            'statusi'    => 'sometimes|in:hapur,ne_proces,mbyllur',
+            'kategoria'  => 'sometimes|string',
+        ]);
+        $tiketi->update($data);
+        return response()->json($tiketi, 200);
+    }
+
+    public function deleteTiketi(Request $request, int $id) {
+        Tiketi::findOrFail($id)->delete();
+        return response()->json("", 204);
+    }
+
+    // ========== Pergjigjet e Tiketave ==========
+    public function getAllPergjigjet(Request $request) {
+        $pergjigjet = PergjigjaTiketi::with('tiketi')->get();
+        return response()->json($pergjigjet, 200);
+    }
+
+    public function createPergjigje(Request $request) {
+        $data = $request->validate([
+            'tiketi_id' => 'required|exists:tiketis,id',
+            'autori'    => 'required|string|max:100',
+            'mesazhi'   => 'required|string',
+            'pergjigja' => 'required|string',
+        ]);
+        $pergjigja = PergjigjaTiketi::create($data);
+        return response()->json($pergjigja, 201);
+    }
+
+    public function deletePergjigje(Request $request, int $id) {
+        PergjigjaTiketi::findOrFail($id)->delete();
+        return response()->json("", 204);
+    }
+
+    // ========== Faturat ==========
+    public function getAllFaturat(Request $request) {
+        $faturat = Faktura::with('klienti')->get();
+        return response()->json($faturat, 200);
+    }
+
+    public function getFaktura(Request $request, int $id) {
+        $faktura = Faktura::with('klienti')->findOrFail($id);
+        return response()->json($faktura, 200);
+    }
+
+    public function createFaktura(Request $request) {
+        $data = $request->validate([
+            'klienti_id'    => 'required|exists:klienti,id',
+            'abonimi_id'    => 'required|exists:abonimi,id',
+            'shuma'         => 'required|numeric|min:0',
+            'data_leshimit' => 'sometimes|date',
+            'data_skadimit' => 'nullable|date',
+            'statusi'       => 'sometimes|in:paguar,papaguar,anuluar',
+            'pershkrimi'    => 'nullable|string',
+        ]);
+        $faktura = Faktura::create($data);
+        return response()->json($faktura, 201);
+    }
+
+    public function updateFaktura(Request $request, int $id) {
+        $faktura = Faktura::findOrFail($id);
+        $data = $request->validate([
+            'klienti_id'    => 'required|exists:klienti,id',
+            'abonimi_id'    => 'required|exists:abonimi,id',
+            'shuma'         => 'required|numeric|min:0',
+            'data_leshimit' => 'sometimes|date',
+            'data_skadimit' => 'nullable|date',
+            'statusi'       => 'sometimes|in:paguar,papaguar,anuluar',
+            'pershkrimi'    => 'nullable|string',
+        ]);
+        $faktura->update($data);
+        return response()->json($faktura, 200);
+    }
+
+    public function deleteFaktura(Request $request, int $id) {
+        Faktura::findOrFail($id)->delete();
         return response()->json("", 204);
     }
 }
